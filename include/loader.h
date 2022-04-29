55522d2ff7e99d925252fb6948d74531d8633095
Florian Hofhammer <florian.hofhammer@epfl.ch>
2022-04-29 14:50:51 +0200

    Switch from mycached to network pipeline simulator
    The network pipeline simulator is currently still not
    compartmentalized.

---
+++ include/loader.h
@@ -0,0 +1,80 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <utils/uthash.h>
+
+#include "mmap_override.h"
+
+struct ip_packet {
+    uint8_t version_ihl;
+    uint8_t dscp_ecn;
+    uint16_t total_length;
+    uint16_t identification;
+    uint16_t flags_fragment_offset;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint32_t source_address;
+    uint32_t dest_address;
+    uint8_t options[40];
+    uint8_t data[65536];
+};
+
+struct udp_packet {
+    uint16_t source_port;
+    uint16_t dest_port;
+    uint16_t length;
+    uint16_t checksum;  // Can be all zeroes in IPv4
+    uint8_t data[65472];
+};
+
+// 64 KiB
+// #define RING_BUFFER_SIZE 65536
+// 1MiB
+#define RING_BUFFER_SIZE 1048576
+
+// TODO variable-sized attributes not part of the packet, calculate on-the-fly
+
+struct translation_NAT {
+    uint16_t in_port;
+    uint32_t out_ip;
+    uint32_t out_port;
+};
+
+#define IP(a, b, c, d) ((uint32_t)(((a) << 24) | ((b) << 16) | ((c) << 8) | (d)))
+#define IP_elems(ip) ((ip) >> 24) & 0xff, ((ip) >> 16) & 0xff, ((ip) >> 8) & 0xff, (ip) & 0xff
+
+// G stands for granularity
+#define G_LOW 0xfff
+#define G_UNIT 0x1000
+
+struct firewall_entry {
+    uint32_t ip;
+    UT_hash_handle hh;
+};
+
+uint32_t blacklist_ip[] = {
+    IP(0, 0, 0, 0),
+    IP(192, 168, 1, 10),
+    IP(255, 255, 255, 255)};
+
+struct translation_NAT translation_units[] = {
+    {10000, IP(192, 184, 121, 48), 17},
+    {10001, IP(192, 83, 128, 40), 17},
+    {10002, IP(192, 18, 92, 123), 17},
+    {10003, IP(192, 47, 99, 39), 17},
+    {10004, IP(192, 82, 91, 3), 17},
+    {10005, IP(192, 31, 91, 9), 17},
+    {10006, IP(192, 73, 1, 4), 17},
+    {10007, IP(192, 83, 23, 9), 17},
+    {10008, IP(192, 83, 91, 73), 17}};
+
+struct NAT_entry {
+    uint16_t in_port;  // id
+    uint32_t out_ip;
+    uint32_t out_port;
+    UT_hash_handle hh;
+};
--- src/main.c
+++ src/main.c
@@ -4,59 +4,159 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
-#include "cache.h"
-#include "cache_wrapper.h"
-#include "config.h"
 #include "bench.h"
+#include "loader.h"
+#include "mmap_override.h"
 
-#define ALPHABETSIZE (26 + 26 + 10 + 2)
+struct ip_packet *ring_buffer;
+struct firewall_entry *firewall;
+struct NAT_entry *nat;
 
-static inline char whichchar(unsigned i) {
-    if (i < 26)
-        return 'A' + i;
-    else if (i < (26 + 26))
-        return 'a' + (i - 26);
-    else if (i < (26 + 26 + 10))
-        return '0' + (i - (26 + 26));
-    else if (i < (ALPHABETSIZE))
-        return ':' + (i - (26 + 26 + 10));
-    else
-        return '\0';
+void print_users() {
+    struct NAT_entry *s;
+
+    for (s = nat; s != NULL; s = s->hh.next) {
+        printf("[%x] %u.%u.%u.%u  %x\n", s->in_port, IP_elems(s->out_ip), s->out_port);
+    }
 }
 
-typedef struct __attribute__((packed)) {
-    int len;
-    char keybuf[60];
-} cachekey_t;
-
-static void make_key(int intkey, cachekey_t *key) {
-    key->len = NCHARS;
-
-    int j;
-    for (j = 0; j < NCHARS; j++) {
-        key->keybuf[j] = whichchar(intkey % ALPHABETSIZE);
-        intkey /= ALPHABETSIZE;
-    }
-    assert(intkey == 0);
-    key->keybuf[j] = '\0';
+struct ip_packet *next_packet(struct ip_packet *p) {  // TODO use bounds
+    size_t address = (size_t)p;
+    address += p->total_length;
+    address += G_UNIT;  // We go to the next 4K-bounded address
+    return (struct ip_packet *)(address & ~G_LOW);
 }
 
-static int encrypt_key(mbedtls_aes_context *ctx, cachekey_t *key, char *buf) {
-    int enclen = 0, remaining;
-    int ret;
-
-    remaining = key->len + sizeof(int);
-    while (remaining > 0) {
-        ret = mbedtls_aes_crypt_ecb(ctx, MBEDTLS_AES_ENCRYPT,
-                                    ((char *)key) + enclen, buf + enclen);
-        if (ret)
-            return -1;
-
-        remaining -= 16;
-        enclen += 16;
+void setup() {
+    firewall = NULL;
+    size_t n_elems_firewall = sizeof(blacklist_ip) / sizeof(uint32_t);
+    for (size_t i = 0; i < n_elems_firewall; i++) {
+        struct firewall_entry *f = malloc(sizeof(struct firewall_entry));
+        uint32_t ip = blacklist_ip[i];
+        f->ip = ip;
+        HASH_ADD_INT(firewall, ip, f);
     }
-    return enclen;
+
+    nat = NULL;
+    size_t n_elems_NAT = sizeof(translation_units) / sizeof(struct translation_NAT);
+    for (size_t i = 0; i < n_elems_NAT; i++) {
+        struct NAT_entry *n = malloc(sizeof(struct NAT_entry));
+        uint32_t in_port = translation_units[i].in_port;
+        n->in_port = in_port;
+        n->out_ip = translation_units[i].out_ip;
+        n->out_port = translation_units[i].out_port;
+        HASH_ADD_INT(nat, in_port, n);
+    }
+}
+
+void print_udp_packet(struct udp_packet *p) {
+    printf("\t%x %x %x\n", p->source_port, p->dest_port, p->length);
+}
+
+void print_ip_packet(struct ip_packet *p, bool options, bool data) {
+    printf("%p: %x %x %x %x %x %x %x %x %u.%u.%u.%u %u.%u.%u.%u\n",
+           p,
+           p->version_ihl,
+           p->dscp_ecn,
+           p->total_length,
+           p->identification,
+           p->flags_fragment_offset,
+           p->ttl,
+           p->protocol,
+           p->header_checksum,
+           IP_elems(p->source_address),
+           IP_elems(p->dest_address));
+    if (options) {
+        for (size_t i = 0; i < 40; i++) {
+            printf("%x", p->options[i]);
+        }
+        printf("\n");
+    }
+    if (data) {
+        print_udp_packet((struct udp_packet *)&p->data);
+    }
+}
+
+uint16_t get_header_checksum(struct ip_packet *p) {
+    uint32_t header_length = (sizeof(struct ip_packet) - 65536) / 16;
+    uint32_t checksum = 0;
+    for (size_t j = 0; j < header_length; j++) {
+        if (j == 5) continue;  // Corresponds to the checksum itself
+        checksum += ((uint16_t *)p)[j];
+    }
+    uint8_t carry = checksum >> 16;
+    checksum &= 0xffff;
+    checksum += carry;
+    checksum += checksum >> 16;
+    checksum &= 0xffff;
+    return checksum;
+}
+
+void ip_generate(struct ip_packet *p, size_t i) {  // TODO give tunable parameters
+    p->version_ihl = (4 << 3) | 15;                // if IHL is less (min 5), we have to reduce the `options` field size
+    p->dscp_ecn = 0;
+    p->total_length = 64 + (i * 91 % 73);  // TODO make it variable // Same as with IHL, but `data` field
+    p->identification = 0;
+    p->flags_fragment_offset = 0;
+    p->ttl = 0;
+    p->protocol = 17;  // Corresponds to UDP, 6 does for TCP
+    p->header_checksum = get_header_checksum(p);
+    switch (i % 4) {
+        case 0:
+            p->source_address = IP(128, 0, 0, 1);
+            break;
+        case 1:
+            p->source_address = IP(255, 255, 255, 255);
+            break;
+        case 2:
+            p->source_address = IP(181, 91, 93, 83);
+            break;
+        default:
+            p->source_address = IP(88, 13, 141, 131);
+            break;
+    }
+    p->dest_address = IP(128, 19, 7, 31);
+    for (size_t j = 0; j < 40; j++) {
+        p->options[j] = 0;
+    }
+
+    // UDP packet generation
+    struct udp_packet *udp_packet = (struct udp_packet *)&p->data;
+    udp_packet->source_port = 0;
+    udp_packet->dest_port = 10000 + (i % 9);
+    udp_packet->length = p->total_length - 64;
+    udp_packet->checksum = 0;
+    for (size_t i = 0; i < udp_packet->length; i++) {
+        udp_packet->data[i] = 0;
+    }
+}
+
+bool valid_address(uint32_t ip) {
+    struct firewall_entry *f;
+    HASH_FIND_INT(firewall, &ip, f);
+    return f == NULL;
+}
+
+bool process_firewall(struct ip_packet *p) {
+    return (p->header_checksum == get_header_checksum(p) && valid_address(p->source_address));
+}
+
+void process_NAT(struct ip_packet *p) {
+    struct NAT_entry *n;
+    uint32_t in_port = ((struct udp_packet *)&p->data)->dest_port;
+    HASH_FIND_INT(nat, &in_port, n);
+
+    if (n != NULL) {
+        p->dest_address = n->out_ip;
+        ((struct udp_packet *)&p->data)->dest_port = n->out_port;
+    }
+    return;
+}
+
+void sink(struct ip_packet *p) {
+    memset(p, 0, (size_t)next_packet(p) - (size_t)p);
 }
 
 int main(int argc, char *argv[]) {
@@ -66,52 +166,44 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    perf_counters_t counters[NPASSES] = { 0 };
+    // TODO process arguments
+    ring_buffer = mmap(0, RING_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
 
-    cachekey_t key;
-    char enckeybuf[256];
-    char encvalbuf[256];
-    mbedtls_aes_context *ctx;
-
-    ctx = wrapper_init();
-
-    /* Set value into cache */
-    for (int i = 0; i < NITEMS; i++) {
-        make_key(i, &key);
-
-        int r = cache_set(key.keybuf, NCHARS, "hell1hell2hell3hell4hell5", 25);
-        assert(r >= 0);
+    if (!ring_buffer) {
+        printf("Allocation of ring buffer failed\n");
+        seL4_TCB_Suspend(seL4_CapInitThreadTCB);
+        return 1;
     }
+    setup();
 
-    /* Retrieve same values into cache */
-    for (int j = 0; j < NPASSES; j++) {
-        for (int i = 0; i < NITEMS; i++) {
-            memset(&key, 0, sizeof(key));
-            make_key(i, &key);
-            int enclen = encrypt_key(ctx, &key, enckeybuf);
-            size_t before_i, after_i, before_c, after_c;
-            RDCTR(before_i, instret);
-            RDCTR(before_c, cycle);
-            cache_get_wrapper(enckeybuf, enclen, encvalbuf, 256);
-            RDCTR(after_c, cycle);
-            RDCTR(after_i, instret);
-            counters[j].instret += after_i - before_i;
-            counters[j].cycle += after_c - before_c;
+    struct ip_packet *end_buf = (struct ip_packet *)((size_t)ring_buffer + RING_BUFFER_SIZE);
+    struct ip_packet *p = (((size_t)ring_buffer & G_LOW) == 0) ? ring_buffer : (struct ip_packet *)(((size_t)ring_buffer + G_UNIT) & ~G_LOW);
+    struct ip_packet *first_packet = p;
+    size_t i = 0;
+
+    for (;;) {
+        // TODO seccells instructions + change function calls by SDSwitch
+        while (p < end_buf) {
+            // SCReval
+            ip_generate(p, i++);
+            // SCGrant & SCTfer
+
+            // SCRecv
+            if (process_firewall(p)) {
+                // SCProt
+                // SCRecv
+                process_NAT(p);
+                // SCTfer
+            }
+
+            print_ip_packet(p, false, true);
+            sink(ring_buffer);  // Consume...
+            // SCInval
+            p = next_packet(p);
         }
-    }
-
-    wrapper_free();
-
-    const char *cumulative_cycles = "Cumulative cycles:";
-    const char *cumulative_instret = "Cumulative retired insns:";
-    const size_t cumulative_str_len = MAX(strlen(cumulative_cycles), strlen(cumulative_instret));
-    for (size_t i = 0; i < NPASSES; i++) {
-        printf("Pass %3zd:\n", i);
-        printf("    %-*s %10zd\n", cumulative_str_len, cumulative_cycles, counters[i].cycle);
-        printf("    %-*s %10zd\n", cumulative_str_len, cumulative_instret, counters[i].instret);
+        p = first_packet;
     }
 
     seL4_TCB_Suspend(seL4_CapInitThreadTCB);
-
     return 0;
 }
