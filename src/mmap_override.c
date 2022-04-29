c9dc23283c0c7c29a874b2700f162a837682827b
Florian Hofhammer <florian.hofhammer@epfl.ch>
2022-04-29 23:15:09 +0200

    Allow specifying base address for mmap

--- src/mmap_override.c
+++ src/mmap_override.c
@@ -1,10 +1,10 @@
 #include "mmap_override.h"
 
+#include <autoconf.h>
 #include <sel4/sel4.h>
 #include <sel4/sel4_arch/mapping.h>
 #include <sel4platsupport/bootinfo.h>
 #include <sel4platsupport/platsupport.h>
-#include <autoconf.h>
 
 #include "alloc.h"
 
@@ -18,27 +18,46 @@ void *mmap_override(void *start, size_t len, int prot, int flags, int fd, off_t 
      */
     seL4_BootInfo *info = platsupport_get_bootinfo();
     static void *addr = (void *)MMAP_BASE;
-    void *ret = addr;
+#ifdef CONFIG_RISCV_SECCELL
+        len = ROUND_UP(len, BIT(seL4_MinRangeBits));
+#else
+        len = ROUND_UP(len, BIT(seL4_PageBits));
+#endif /* CONFIG_RISCV_SECCELL */
+    if (start == NULL) {
+        /* No start address passed => use the available address range from MMAP_BASE on */
+        start = addr;
+        addr = (void *)((char *)addr + len);
+    }
+    void *ret = start;
     seL4_Error error;
 #ifdef CONFIG_RISCV_SECCELL
     seL4_CPtr range = alloc_object(info, seL4_RISCV_RangeObject, len);
-    error = seL4_RISCV_Range_Map(range, seL4_CapInitThreadVSpace, (seL4_Word)addr, seL4_ReadWrite,
+    error = seL4_RISCV_Range_Map(range, seL4_CapInitThreadVSpace, (seL4_Word)start, seL4_ReadWrite,
                                  seL4_RISCV_Default_VMAttributes);
-    ZF_LOGF_IF(error != seL4_NoError, "Failed to map range @ %p", addr);
-    addr = (void *)((char *)addr + len);
+    if (unlikely(error != seL4_NoError)) {
+        ZF_LOGF("Failed to map range @ %p", start);
+        return NULL;
+    }
 #else
     /* Map page table structures */
-    for (; (size_t)addr < (size_t)ret + len; addr += BIT(seL4_PageTableBits + seL4_PageTableIndexBits)) {
+    for (; (size_t)start < (size_t)ret + len; start += BIT(seL4_PageTableBits + seL4_PageTableIndexBits)) {
         seL4_CPtr pagetable = alloc_object(info, seL4_RISCV_PageTableObject, seL4_PageTableBits);
-        error = seL4_RISCV_PageTable_Map(pagetable, seL4_CapInitThreadVSpace, (seL4_Word)addr,
+        error = seL4_RISCV_PageTable_Map(pagetable, seL4_CapInitThreadVSpace, (seL4_Word)start,
                                          seL4_RISCV_Default_VMAttributes);
+        if (unlikely(error != seL4_NoError)) {
+            ZF_LOGF("Failed to map page table @ %p", start);
+            return NULL;
+        }
     }
     /* Map actual pages */
-    for (addr = ret; (size_t)addr < (size_t)ret + len; addr += BIT(seL4_PageBits)) {
+    for (start = ret; (size_t)start < (size_t)ret + len; start += BIT(seL4_PageBits)) {
         seL4_CPtr page = alloc_object(info, seL4_RISCV_4K_Page, seL4_PageBits);
-        error = seL4_RISCV_Page_Map(page, seL4_CapInitThreadVSpace, (seL4_Word)addr, seL4_ReadWrite,
+        error = seL4_RISCV_Page_Map(page, seL4_CapInitThreadVSpace, (seL4_Word)start, seL4_ReadWrite,
                                     seL4_RISCV_Default_VMAttributes);
-        ZF_LOGF_IF(error != seL4_NoError, "Failed to map page @ %p", addr);
+        if (unlikely(error != seL4_NoError)) {
+            ZF_LOGF("Failed to map page @ %p", start);
+            return NULL;
+        }
     }
 #endif /* CONFIG_RISCV_SECCELL */
     return ret;
